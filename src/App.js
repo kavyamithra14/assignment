17fe8c8016dca2d843315b7df0dfe45f531e9806
kavyamithra <mithrakavya142@gmail.com>
2019-03-26 16:41:01 +0530

    created home page and listed all posts

--- src/App.js
+++ src/App.js
@@ -1,33 +1,56 @@
 import React, { Component } from 'react';
 
-
+let pStyle = {
+  fontSize: '15px',
+  textAlign: 'center',
+  color:'#00a3cc'
+};
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      hits: [],
+      data:[],
+     
     };
   }
+  
+
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
-      .then(data => this.setState({ hits: data.hits }));
+      .then(data =>{
+        console.log(data);
+        let info=data.map((item)=>{
+          return (
+              <div key={item.id}>
+              <p style={pStyle}>{item.id}</p>
+                <p>{item.userId}</p>
+                <p>{item.title}</p>
+                <p>{item.body}</p>
+
+              </div>
+
+            )
+        
+       })
+        
+        this.setState({data:info});
+
+      })
+
+       .catch(error=>console.log("error",error));
+
   }
 
   render() {
-    const { hits } = this.state;
-
+    
     return (
       <div>
-      
-      {hits.map(hit =>
-           id={hit.id}
-            
-         
-    )}
+      <h1>ALL POSTS</h1>
+      {this.state.data}
       
       </div>
     );
